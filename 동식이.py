05b594e057dcf40f46d0c84f83fad4fd13b5731f
pici1110 <90972595+pici1110@users.noreply.github.com>
2021-09-18 23:10:09 +0900

    Add files via upload

---
+++ 동식이.py
@@ -0,0 +1,35 @@
+import discord
+
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print('login')
+    print(client.user.id)
+    print('-----------------------------------')
+    game = discord.Game("동식이다냥")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("동식아 들어와"):
+        await message.channel.send("집에 들어간다냥")
+    if message.content.startswith("동식아 사랑해"):
+        await message.channel.send("나도 사랑한다냥")
+    if message.content.startswith("동식아 뭐해?"):
+        await message.channel.send("젤리 빨래 한다냥")
+    if message.content.startswith("동식아 행복해?"):
+        await message.channel.send("완전 행복하다냥!")
+    if message.content.startswith("동식아 발"):
+        await message.channel.send("여기 있다냥")
+    if message.content.startswith("동식아 손"):
+        await message.channel.send("여기 있다냥!")
+    if message.content.endswith("동식아"):
+        await message.channel.send("냥?")
+    if message.content.startswith("동식아 안녕"):
+        await message.channel.send("안녕하다냥")
+    if message.content.startswith("동식아 쓰담쓰담"):
+        await message.channel.send("기분 좋다냥")
+
+bot.run('ODg4MDgwMzQyNjEzOTAxMzUy.YUNfRQ.ZJb5HTtMzjRE6g4TyzuxWef7uew')
